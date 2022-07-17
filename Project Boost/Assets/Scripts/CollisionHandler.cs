c86839fddaea61335413efee4df0592ef10d847f
SlamiTwosti <luchtgames@gmail.com>
2022-07-17 23:09:03 +0200

    Gekke next level loading systeem ding wowowow
    bingus bongos you lose your cockus

--- Project Boost/Assets/Scripts/CollisionHandler.cs
+++ Project Boost/Assets/Scripts/CollisionHandler.cs
@@ -1,9 +1,12 @@
 using System;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    [SerializeField] private float levelLoadDelay = 1f;
+
     private void OnCollisionEnter(Collision collision)
     {
         switch (collision.gameObject.tag)
@@ -12,14 +15,47 @@ public class CollisionHandler : MonoBehaviour
                 Debug.Log("Hit Friendly");
                 break;
             case "Finish":
-                Debug.Log("Good Job! :)");
-                break;
-            case "Fuel":
-                Debug.Log("You picked up fuel!");
+                StartSuccessSequence();
                 break;
             default:
-                Debug.Log("You blew it!");
+                StartCrashSequence();
                 break;
         }
     }
+
+    private void StartSuccessSequence()
+    {
+        // todo add SFX upon crash
+        // todo add particle effect upon crash
+        GetComponent<Movement>().enabled = false;
+        Invoke("LoadNextLevel", levelLoadDelay);
+    }
+    
+    private void StartCrashSequence()
+    {
+        // todo add SFX upon crash
+        // todo add particle effect upon crash
+        GetComponent<Movement>().enabled = false;
+        Invoke("ReloadLevel", levelLoadDelay);
+    }
+    
+    private void LoadNextLevel()
+    {
+        Debug.Log("Good Job! :)");
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+    
+    private void ReloadLevel()
+    {
+        Debug.Log("You blew it!");
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+    
 }
